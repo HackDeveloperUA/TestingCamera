92307f64c463ba0c4cdd3fac81a5d1e2b0c79b4a
HackDeveloperUA <hack@mac-mini-hack.local>
2015-11-02 11:09:42 +0300

    Initial Commit

---
+++ TestingCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestingCamera
+//
+//  Created by Hack on 02.11.15.
+//  Copyright © 2015 ttconsalting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestingCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestingCamera
+//
+//  Created by Hack on 02.11.15.
+//  Copyright © 2015 ttconsalting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
